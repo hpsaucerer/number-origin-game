32c8bb4824d661aec9306a3412b59ce43e105004
hpsaucerer <martin-beattie@outlook.com>
2025-03-31 18:23:30 +0100

    Fix import path for Documents.js

--- pages/document.js
+++ pages/document.js
@@ -1,6 +1,16 @@
-<Head>
-  <link
-    href="https://fonts.googleapis.com/css2?family=Days+One&display=swap"
-    rel="stylesheet"
-  />
-</Head>
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* custom fonts, meta tags etc */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
