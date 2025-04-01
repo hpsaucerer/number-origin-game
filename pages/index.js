6374bce491f7a572f313885be3b33647672606f8
hpsaucerer <martin-beattie@outlook.com>
2025-04-01 21:36:48 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -433,20 +433,21 @@ const renderPill = ({ label, color, tooltip, icon, index }) => (
 </div>
 
         
-          <ul className="list-decimal ml-6">
-            <li>
-              <strong>Look at the number.</strong>< br/>
-              What could it mean?< br/>< br/>
-            </li>
-            <li>
-              <strong>Make a guess. You have 4 in total.</strong>< br/>
-              Type what you think the number relates to (e.g., 'speed of light', 'year of the moon landing'.< br/>< br/>
-            </li>
-            <li>
-              <strong>Stuck? Reveal a clue to help you. </strong>< br/>
-              Remember though, this uses up a guess.< br/>< br/>
-            </li>
-          </ul>
+<ul className="list-decimal ml-6">
+  <li className="mb-2">
+    <strong>Look at the number.</strong><br />
+    What could it mean?
+  </li>
+  <li className="mb-2">
+    <strong>Make a guess. You have 4 in total.</strong><br />
+    Type what you think the number relates to (e.g., "speed of light", "year of the moon landing").
+  </li>
+  <li>
+    <strong>Stuck? Reveal a clue to help you.</strong><br />
+    Remember though, this uses up a guess.
+  </li>
+</ul>
+
 
 <div className="mt-4">
   <h3 className="text-lg font-bold text-black mb-2">Categories</h3>
