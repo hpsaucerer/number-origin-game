385e041dc337561e67ffcfee24e12c004a251d84
hpsaucerer <martin-beattie@outlook.com>
2025-04-03 18:23:10 +0100

    Update FunFactBox.js

--- components/FunFactBox.js
+++ components/FunFactBox.js
@@ -7,7 +7,7 @@ export default function FunFactBox({ text }) {
 <div className="mt-4 flex items-start gap-4 p-4 bg-yellow-100 text-yellow-900 border-l-4 border-yellow-400 rounded shadow">
   <div className="w-[64px] h-[64px] flex items-center justify-center">
     <img
-      src="/icons/funfact.png"
+      src="/icons/funfact.svg"
       alt="Fun Fact Icon"
       className="w-full h-full object-contain"
     />
