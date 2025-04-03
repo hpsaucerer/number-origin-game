5bf8b1c87bf92f0d32c3a7c6cbc56d78beb4e468
hpsaucerer <martin-beattie@outlook.com>
2025-04-03 22:10:38 +0100

    Update FunFactBox.js

--- components/FunFactBox.js
+++ components/FunFactBox.js
@@ -3,15 +3,16 @@ import React from "react";
 export default function FunFactBox({ text }) {
   if (!text) return null;
 
-return (
-  <div className="flex items-center bg-yellow-100 text-yellow-900 px-3 py-2 rounded border-l-4 border-yellow-400 shadow-sm">
-    <img
-      src="/icons/funfact.svg"
-      alt="Fun Fact"
-      className="inline-block w-5 h-5 mr-2"
-    />
-    <p className="text-sm leading-snug m-0 p-0">{text}</p>
-  </div>
-);
+  return (
+    <div className="flex items-center bg-yellow-100 text-yellow-900 px-3 py-2 rounded border-l-4 border-yellow-400 shadow-sm">
+      <img
+        src="/icons/funfact.svg"
+        alt="Fun Fact"
+        className="inline-block w-5 h-5 mr-2"
+      />
+      <p className="text-sm leading-snug m-0 p-0">{text}</p>
+    </div>
+  );
+}
 
 
