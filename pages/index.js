36f5d3e1b87c8d384598263fc26ac584bfeed082
hpsaucerer <martin-beattie@outlook.com>
2025-04-04 18:15:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -575,7 +575,7 @@ const renderCategoryPills = () => {
 
       {/* Stats Popup */}
 <Dialog open={showStats} onOpenChange={setShowStats}>
-<DialogContent className="relative bg-white pt-10 pr-10 pb-6 pl-6 rounded-xl shadow-xl w-full max-w-md">
+<DialogContent className="relative bg-white pt-10 pr-10 pb-6 pl-6 rounded-xl shadow-xl w-full max-w-md mx-auto">
     <button
       className="absolute top-4 right-4 text-blue-500 hover:text-blue-600 transition z-50"   
       onClick={() => setShowStats(false)}
