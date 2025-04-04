35ad58a58b2ca764134533afc3e6188013d9cefd
hpsaucerer <martin-beattie@outlook.com>
2025-04-04 10:27:47 +0100

    Update PostGameModal.js

--- components/PostGameModal.js
+++ components/PostGameModal.js
@@ -48,7 +48,7 @@ useEffect(() => {
         {/* ❌ Close Button */}
         <button
         style={{ border: "2px solid red" }}
-          className="absolute top-2 right-2 text-gray-500 hover:text-black"
+          className="absolute top-2 right-2 z-50 text-gray-500 hover:text-black"
           onClick={onClose}
           aria-label="Close"
         >
