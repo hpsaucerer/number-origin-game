26b9946cea50a2692e3453a9c5da7951148e703c
hpsaucerer <martin-beattie@outlook.com>
2025-04-04 16:44:56 +0100

    Update PostGameModal.js

--- components/PostGameModal.js
+++ components/PostGameModal.js
@@ -53,7 +53,7 @@ export default function PostGameModal({ open, onClose, isCorrect, stats, puzzle 
     {/* Header and content go here */}
 <DialogHeader className="text-center mb-4">
   <DialogTitle className="text-2xl font-bold font-vietnam">
-    {isCorrect ? "Knocked it out the park! ⚾" : "Better luck tomorrow!"}
+    {isCorrect ? "Knocked it out of the park! ⚾" : "Ah, better luck tomorrow!"}
   </DialogTitle>
 </DialogHeader>
 
