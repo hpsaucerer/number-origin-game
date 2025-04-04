5a3afeafae548a65daa4f916a9c91ba2d89d4756
hpsaucerer <martin-beattie@outlook.com>
2025-04-04 18:06:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -577,7 +577,7 @@ const renderCategoryPills = () => {
 <Dialog open={showStats} onOpenChange={setShowStats}>
   <DialogContent className="relative bg-white pt-10 pr-10 pb-6 pl-6 rounded-xl shadow-xl">
     <button
-      className="absolute top-2 right-2 text-blue-500 hover:text-blue-600 transition z-50"   
+      className="absolute top-3 right-3 text-blue-500 hover:text-blue-600 transition z-50"   
       onClick={() => setShowStats(false)}
       aria-label="Close"
     >
