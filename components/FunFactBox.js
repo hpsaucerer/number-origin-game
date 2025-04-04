c869b981e33d93e4c11a10ecbcde96ad22224945
hpsaucerer <martin-beattie@outlook.com>
2025-04-04 11:00:24 +0100

    Update FunFactBox.js

--- components/FunFactBox.js
+++ components/FunFactBox.js
@@ -4,15 +4,16 @@ export default function FunFactBox({ puzzle }) {
   if (!puzzle?.funFact) return null;
 
   return (
-    <div className="mt-4 flex flex-col items-center p-4 bg-yellow-100 text-yellow-900 border-l-4 border-yellow-400 rounded-lg shadow-sm text-center">
-      <img
-        src="/icons/funfact.svg"
-        alt="Fun Fact Icon"
-        className="w-24 h-24 -mb-2"
-      />
-      <p className="text-sm leading-snug max-w-sm">
-        {puzzle.funFact}
-      </p>
-    </div>
+<div className="mt-4 flex flex-col items-center pt-2 pb-4 px-4 bg-yellow-100 text-yellow-900 border-l-4 border-yellow-400 rounded-lg shadow-sm text-center">
+  <img
+    src="/icons/funfact.svg"
+    alt="Fun Fact Icon"
+    className="w-24 h-24 mt-0 -mb-2"
+  />
+  <p className="text-sm leading-snug max-w-sm">
+    {puzzle.funFact}
+  </p>
+</div>
+
   );
 }
