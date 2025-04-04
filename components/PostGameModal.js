d054ddfff6174da548c7071faf184cd08c327fd1
hpsaucerer <martin-beattie@outlook.com>
2025-04-04 10:25:39 +0100

    Update PostGameModal.js

--- components/PostGameModal.js
+++ components/PostGameModal.js
@@ -47,6 +47,7 @@ useEffect(() => {
       <DialogContent className="max-w-md p-4 relative">
         {/* ❌ Close Button */}
         <button
+        style={{ border: "2px solid red" }}
           className="absolute top-2 right-2 text-gray-500 hover:text-black"
           onClick={onClose}
           aria-label="Close"
