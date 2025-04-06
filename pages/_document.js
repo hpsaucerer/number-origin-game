06bbec993db6462c0fa802ffe3da819443e27b15
hpsaucerer <martin-beattie@outlook.com>
2025-04-06 17:32:55 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* custom fonts, meta tags etc */}
+
+ {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* Optional: Apple and PNG versions for better device support */}
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
+    
       </Head>
       <body>
         <Main />
