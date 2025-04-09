25fcdda3bde4b51782ce598fb67d3e62d3f0d626
hpsaucerer <martin-beattie@outlook.com>
2025-04-09 12:10:44 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -376,7 +376,7 @@ const renderCategoryPills = () => {
   <img
     src={`/icons/${cat.label.toLowerCase()}.png`}
     alt={`${cat.label} icon`}
-    className="w-4 h-4 mr-1"
+    className="w-8 h-8 mr-1"
     style={{ marginTop: '-1px' }}
   />
   {cat.label}
