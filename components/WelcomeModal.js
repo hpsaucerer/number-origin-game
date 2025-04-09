fc042e70615f4128f2ae742683f59069a3a85e60
hpsaucerer <martin-beattie@outlook.com>
2025-04-09 13:48:52 +0100

    Update WelcomeModal.js

--- components/WelcomeModal.js
+++ components/WelcomeModal.js
@@ -6,7 +6,8 @@ export default function WelcomeModal({ open, onOpenChange, showTutorial, setShow
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
 
-      <DialogContent>
+      <DialogContent className="w-full max-w-[480px] mx-auto rounded-xl">
+
 <DialogHeader>
   <p className="text-xl font-bold text-center">
     Numerus
