52a08ef9d3da44e9c2637227409716da8578af55
hpsaucerer <martin-beattie@outlook.com>
2025-04-11 11:10:39 +0100

    Update OnScreenKeyboard.js

--- components/OnScreenKeyboard.js
+++ components/OnScreenKeyboard.js
@@ -59,7 +59,7 @@ export default function OnScreenKeyboard({ onKeyPress }) {
 
         <button
           onClick={() => onKeyPress("↵")}
-          className="bg-[#2ECC71] text-white px-3 py-2 rounded text-sm font-medium flex-1"
+          className="bg-[#63c4a7] text-white px-3 py-2 rounded text-sm font-medium flex-1"
         >
           Enter
         </button>
