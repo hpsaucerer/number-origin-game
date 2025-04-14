77a9e670da933210a1ef624ad27a486248142b13
hpsaucerer <martin-beattie@outlook.com>
2025-04-14 09:27:01 +0100

    Create newPuzzles.js

---
+++ data/newPuzzles.js
@@ -0,0 +1,12 @@
+const newPuzzles = [
+  {
+    number: 1515,
+    formatted: "1,515",
+    answer: "Sample Answer",
+    clues: ["Clue one", "Clue two", "Clue three"],
+    keywords: ["sample", "puzzle"],
+    funFact: "This is a sample fun fact."
+  }
+];
+
+export default newPuzzles;
