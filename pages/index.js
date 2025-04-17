766e6d5f4e819f7068dea623a1c3183c6c0f13f3
hpsaucerer <martin-beattie@outlook.com>
2025-04-17 12:02:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -430,7 +430,7 @@ const renderCategoryPills = () => {
   <img
     src="/logo.svg"
     alt="Game Logo"
-    className="h-18 sm:h-16 md:h-20 lg:h-28 xl:h-36 w-auto translate-y-3"
+    className="h-18 sm:h-16 md:h-20 lg:h-28 xl:h-36 w-auto translate-y-4"
   />
 </Link>
 
