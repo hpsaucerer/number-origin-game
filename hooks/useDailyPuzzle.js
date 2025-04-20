7a73fa55898fcb2e5915cbe807a2f34d78029618
hpsaucerer <martin-beattie@outlook.com>
2025-04-20 09:57:32 +0100

    Update useDailyPuzzle.js

--- hooks/useDailyPuzzle.js
+++ hooks/useDailyPuzzle.js
@@ -4,13 +4,14 @@ export const useDailyPuzzle = (puzzles) => {
   const [puzzle, setPuzzle] = useState(null);
   const [puzzleNumber, setPuzzleNumber] = useState(null);
 
+  const LAUNCH_DATE = process.env.NEXT_PUBLIC_LAUNCH_DATE;
+
   useEffect(() => {
-    const today = "2025-04-15"; // ⬅️ Match this to your test puzzle's date
+    const today = LAUNCH_DATE || new Date().toISOString().split("T")[0];
     const todayPuzzle = puzzles.find((p) => p.date === today);
 
     if (todayPuzzle) {
       setPuzzle(todayPuzzle);
-      // Get index in case you still want to show puzzle #n
       const index = puzzles.findIndex((p) => p.date === today);
       setPuzzleNumber(index + 1);
     } else {
