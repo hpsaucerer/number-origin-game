636ed4d0a1fa60b17faef8e976858062ba12182f
hpsaucerer <martin-beattie@outlook.com>
2025-04-30 21:12:18 +0100

    Create page.js

---
+++ pages/tools/page.js
@@ -0,0 +1,9 @@
+import MetadataGenerator from "@/components/MetadataGenerator";
+
+export default function AdminToolsPage() {
+  return (
+    <div className="p-8 min-h-screen bg-white">
+      <MetadataGenerator />
+    </div>
+  );
+}
