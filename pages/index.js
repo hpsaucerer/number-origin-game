3e65be418fdc871358c37b690f4fb188f9c9466c
hpsaucerer <martin-beattie@outlook.com>
2025-05-07 16:26:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -735,7 +735,7 @@ const acceptableFuse = new Fuse(
     const hasOnlyEssentialMatch = hasStrongMatch && uniqueEssentialMatchCount >= 2;
 
     const lenientFuzzyPass =
-  bestMatch?.score <= 0.55 &&
+  bestMatch?.score <= 0.575 &&
   matchedRequired.length >= 2 &&
   cleanedGuess.length > 12;
 
