7f52348868fcb32d6821e77493576f85e1ff3e86
hpsaucerer <martin-beattie@outlook.com>
2025-05-13 21:02:01 +0100

    Update CommunityBox.js

--- components/CommunityBox.js
+++ components/CommunityBox.js
@@ -6,7 +6,7 @@ const CommunityBox = () => {
     <div className="bg-blue-50 border border-blue-200 rounded-lg px-4 pt-2 pb-4 shadow-sm mt-4 text-center">
       <div className="flex flex-col items-center gap-1 mb-2">
         <img
-          src="/icons/numerus-community.png" // adjust if needed
+          src="/icons/numerus-community-logo.png" // adjust if needed
           alt="Community Logo"
           className="w-32 sm:w-26 h-auto object-contain"
         />
