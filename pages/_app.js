7feb92ce0edd408845700216389df4957d4ecc76
hpsaucerer <martin-beattie@outlook.com>
2025-05-14 20:43:52 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -34,7 +34,7 @@ export default function App({ Component, pageProps }) {
 
         {/* Optional Open Graph tags for social previews */}
         <meta property="og:title" content="Numerus – The Daily Reverse Trivia Game" />
-        <meta property="og:description" content="Can you guess what today's number signifies? Use clues to solve the puzzle!" />
+        <meta property="og:description" content="Can you guess what today's number represents? Use clues to solve the puzzle!" />
         <meta property="og:image" content="/social-preview.png" />
         <meta property="og:url" content="https://numerus.site" />
         <meta name="twitter:card" content="summary_large_image" />
