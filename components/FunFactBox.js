4b8392e3596c8d1d2bb008022470b956cb15be23
hpsaucerer <martin-beattie@outlook.com>
2025-05-17 16:33:47 +0100

    Update FunFactBox.js

--- components/FunFactBox.js
+++ components/FunFactBox.js
@@ -9,7 +9,7 @@ export default function FunFactBox({ puzzle }) {
     <div className="mt-4 px-4">
       <div className="max-w-prose mx-auto flex flex-col items-center pt-2 pb-4 px-4 bg-yellow-100 text-yellow-900 border-l-4 border-yellow-400 rounded-lg shadow-sm text-center">
         <img
-          src="/icons/funfact.svg"
+          src="/icons/nugget-knowledge.png"
           alt="Fun Fact Icon"
           className="w-24 h-24 -mt-6 -mb-2"
         />
