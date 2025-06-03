9759861092a92ed59265b84d3648b21ac91f34a3
hpsaucerer <martin-beattie@outlook.com>
2025-06-03 19:25:26 +0100

    Update PostGameModal.js

--- components/PostGameModal.js
+++ components/PostGameModal.js
@@ -279,7 +279,7 @@ export default function PostGameModal({
           </div>
         </div>
       {showLeaderboard && (
-  <Leaderboard onClose={() => setShowLeaderboard(false)} puzzleId={puzzle.date} />
+  <Leaderboard onClose={() => setShowLeaderboard(false)} puzzleDate={new Date(puzzle.date).toISOString().split("T")[0]} />
 )}
       </DialogContent>
     </Dialog>
