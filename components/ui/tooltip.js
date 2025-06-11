84a1a8c0be0e0cf50eb3d1d3a769cdf2ec1e6aaf
hpsaucerer <martin-beattie@outlook.com>
2025-06-11 17:49:58 +0100

    Update tooltip.js

--- components/ui/tooltip.js
+++ components/ui/tooltip.js
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
-import { cn } from "@/lib/utils";
 
 export const Tooltip = ({ children, ...props }) => (
   <TooltipPrimitive.Provider>
@@ -12,15 +11,15 @@ export const Tooltip = ({ children, ...props }) => (
 
 export const TooltipTrigger = TooltipPrimitive.Trigger;
 
-export const TooltipContent = ({ className, side = "top", ...props }) => (
+export const TooltipContent = ({ className = "", side = "top", ...props }) => (
   <TooltipPrimitive.Portal>
     <TooltipPrimitive.Content
       side={side}
       align="center"
-      className={cn(
-        "z-50 rounded-md border border-gray-100 bg-white px-2 py-1 text-xs text-gray-800 shadow-md",
+      className={
+        `z-50 rounded-md border border-gray-100 bg-white px-2 py-1 text-xs text-gray-800 shadow-md ` +
         className
-      )}
+      }
       {...props}
     />
   </TooltipPrimitive.Portal>
