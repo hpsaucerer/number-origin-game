b4eef93a000b9510ea3eb50c1a285985e8056d3e
hpsaucerer <martin-beattie@outlook.com>
2025-06-26 15:22:01 +0100

    Update NumberHistoryWheel.js

--- components/NumberHistoryWheel.js
+++ components/NumberHistoryWheel.js
@@ -42,7 +42,7 @@ export default function NumberHistoryWheel({ history }) {
               alt=""
               className="
                 pointer-events-none
-                absolute -top-2 -right-2
+                absolute -top-0 -right-0
                 w-20
                 filter brightness-0 saturate-100 opacity-15
               "
